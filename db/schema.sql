53dde72d2fe310590bba40049274600a6600fa59
Arms-like-trees <117699558+Arms-like-trees@users.noreply.github.com>
2023-03-15 12:07:02 -0500

    starts tables

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,14 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+
+CREATE TABLE category (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    category_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE product (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    product_name VARCHAR(255) NOT NULL,
+    price DECIMAL(11, 10)
+)
